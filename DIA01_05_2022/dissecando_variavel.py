9aede9ba92f36a556380d93be75b43f489c989fe
Flipe124 <flipeoliveria007@gmail.com>
2022-05-01 21:43:21 -0300

    v1

---
+++ DIA01_05_2022/dissecando_variavel.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+entrada = input("Digite algo: ")
+print("O tipo primitivo desse valor é ", type(entrada))
+print("Só tem espaços? ", entrada.isspace())
+print("É um número? ", entrada.isnumeric())
+print("É alfabético? ", entrada.isalpha())
+print("É alfanumérico? ", entrada.isalnum())
+print("Está em maiúuscula ?", entrada.isupper())
+print("Está minúscula? ", entrada.islower())
+print("Está capitalizado: ", entrada.istitle())
+
+
+
+
--- DIA01_05_2022/teste.py
+++
@@ -1,6 +0,0 @@
-print("Hello world")
-
-entrada = input("Digite algo: ")
-print("O tipo primitivo desse valor é ", type(entrada))
-print("Só tem espaços? ", entrada.isspace())
-
