700725a9480d59e063c2ae1b74e92d8067d9ddca
Flipe124 <flipeoliveria007@gmail.com>
2022-05-03 16:20:10 -0300

    v1

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+a = 5+2   # Soma
+b = 5-2   # Subtração
+c = 5*2   # Multiplicação
+d = 5/2   # Divisão
+e = 5**2  # Potência 
+f = 5//2  # Divisão inteira
+g = 5%2   # Resto da divisão
+
+print("Soma: ", a)
+print("Subtração: ", b)
+print("Multiplicação: ",c)
+print("Divisão: ", d)
+print("Potência: ", e)
+print("Divisão inteira:", f)
+print("Resto da divisão: ", g)
+
+# Ordem de precedência:
+# (1) ()
+# (2) **
+# (3) * / // %
+# (4) + -
